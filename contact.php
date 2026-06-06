4da75622de8b0c8eea531ddbcc8f8b49e56ead29
aliefian880 <aliefiansubagia803@gmail.com>
2026-06-06 12:45:54 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php include 'views/header.php'; ?>
+
+<h2>Halaman Contact</h2>
+
+<form method="POST">
+
+    <label>Nama</label><br>
+    <input type="text" name="nama"><br><br>
+
+    <label>Email</label><br>
+    <input type="email" name="email"><br><br>
+
+    <button type="submit">
+        Kirim
+    </button>
+
+</form>
+
+<?php include 'views/footer.php'; ?>
