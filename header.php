935521306d6c8d7b02ae9808d8aeb9c61c5272a9
aliefian880 <aliefiansubagia803@gmail.com>
2026-06-06 12:38:19 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Website MVC</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h1>Website MVC PHP</h1>
+
+    <nav>
+        <a href="?page=home">Home</a> |
+        <a href="?page=contact">Contact</a>
+    </nav>
+
+    <hr>
+</header>
---
+++ home.php
@@ -0,0 +1,9 @@
+<?php include 'views/header.php'; ?>
+
+<h2>Halaman Home</h2>
+
+<p>
+Selamat datang di website MVC sederhana menggunakan PHP.
+</p>
+
+<?php include 'views/footer.php'; ?>
